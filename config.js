53f5f7e52b52e4c68d87f6f51eba242014b516f8
CagataySonmez <cagatay.sonmez@boun.edu.tr>
2016-06-08 19:50:25 +0300

    config file is converted from json to js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-﻿{
+﻿module.exports= {
     "connectionString": "mongodb://localhost:27017/euro2016_prediction",
-    "apiUrl": "http://localhost:3000/api",
+    "apiUrl": "http://localhost:" + process.env.PORT +  "/api",
     "secret": "arcelik beylikduzu network team"
 }
